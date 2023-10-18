7eacb9343fb6a1ae1fb15c57a9f11d54847840ff
BenoistP <bp_github.com@gazouille.net>
2023-10-18 17:58:30 +0200

    app migration

--- frontend/src/app/js/ui/themes/themes.ts
+++
@@ -1,5 +0,0 @@
-// Consts
-
-const THEMES_NAMES: string[] =  ['dark', 'light', 'realOrange', /* 'blue' */];
-
-export default THEMES_NAMES;
--- frontend/src/app/ui/ContentBottomPadding.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export const ContentBottomPadding = () => {
-
-    return (
-        <div className="w-full flex-1" id="bottom">
-        </div>
-    );
-};
