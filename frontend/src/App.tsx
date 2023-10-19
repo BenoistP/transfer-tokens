748b2cca2fdc9f192d5f13e0da679e2e630e7c90
BenoistP <bp_github.com@gazouille.net>
2023-10-19 15:21:04 +0200

    taiulwind, daisyui

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -28,8 +28,8 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
-      <h1 className="text-3xl font-bold underline">
-        tailwind Hello world!
+      <h1 className="text-3xl font-bold underline bg-warning text-info">
+        tailwind+daisyui Hello world!
       </h1>
     </>
   )
