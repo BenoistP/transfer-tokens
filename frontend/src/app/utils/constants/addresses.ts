ad6431483eb19250c7d8b2ab4092d0632b4292e1
BenoistP <bp_github.com@gazouille.net>
2023-10-19 13:41:29 +0200

    vite, tailwind

--- frontend/src/app/utils/constants/addresses.ts
+++
@@ -1,4 +0,0 @@
-export const NULL_ADDRESS = "0x";
-export const DEFAULT_TARGET_ADDRESS = NULL_ADDRESS;
-export const ADDRESS_MIN_SIZE = 40;
-export const ADDRESS_MAX_SIZE = ADDRESS_MIN_SIZE+2; // '0x'
