a45aaebee365c027efa1f4a21e3c173c218613ea
BenoistP <bp_github.com@gazouille.net>
2023-10-19 16:29:28 +0200

    config

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,7 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
-import './index.css'
+
+import '@styles/global.css'
+import '@styles/tailwind.css'
+// import '@css/global.css'
+// import '@css/tailwind.css'
+// import '/src/app/styles/global.css'
+// import '/src/app/styles/tailwind.css'
+
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
