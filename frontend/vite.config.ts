a035904296e48775be01f4db77e402fee9023dac
BenoistP <bp_github.com@gazouille.net>
2023-10-20 18:08:36 +0200

    env

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,6 +1,9 @@
 import { defineConfig } from 'vite'
 import tsconfigPaths from 'vite-tsconfig-paths'
 import react from '@vitejs/plugin-react'
+// import dotenv from 'dotenv';
+
+// dotenv.config(); // load env vars from .env
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -10,6 +13,13 @@ export default defineConfig({
   ],
   server: {
     port: 8000,
-    open: "/public/index.html"
-  }
+    open: false,
+  },
+  envPrefix: ['PUBLIC_', 'APIKEY_'],
+  // define: {
+  //   ALCHEMY_APIKEY: `"${process.env.ALCHEMY_APIKEY}"`,
+  //   INFURA_APIKEY: `"${process.env.INFURA_APIKEY}"`,
+  //   WALLET_CONNECT_APIKEY: `"${process.env.WALLET_CONNECT_APIKEY}"`,
+  // },
+
 })
