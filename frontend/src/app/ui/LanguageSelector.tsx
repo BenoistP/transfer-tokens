31ad5a851721a0284f6e39c2b83dbe9baa9c7730
BenoistP <bp_github.com@gazouille.net>
2023-10-20 10:37:40 +0200

    react-cookie

--- frontend/src/app/ui/LanguageSelector.tsx
+++ frontend/src/app/ui/LanguageSelector.tsx
@@ -1,5 +1,4 @@
-import React,
- {useCallback, useEffect, useState} from "react";
+import {useCallback, useEffect, useState} from "react";
 import { useTranslation } from "react-i18next";
 import { useCookies } from "react-cookie";
 import { COOKIE_LANGUAGE } from "~/utils/constants/misc";
