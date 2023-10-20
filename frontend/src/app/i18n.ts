98360c5186e56a0b6f99aefb341971a3ce58c8f0
BenoistP <bp_github.com@gazouille.net>
2023-10-20 16:54:37 +0200

    Update i18n.ts

--- frontend/src/app/i18n.ts
+++ frontend/src/app/i18n.ts
@@ -20,8 +20,8 @@ i18n
   // init i18next
   // for all options read: https://www.i18next.com/overview/configuration-options
   .init({
-    fallbackLng: 'en',
-    debug: true,
+    fallbackLng: ['en', 'fr'],
+    // debug: true,
     backend: {
       loadPath: '/locales/{{ns}}-common-{{lng}}.json'
     },
