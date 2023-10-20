e37c8d93eae959671fbb7fdd56b262b8d8d88600
BenoistP <bp_github.com@gazouille.net>
2023-10-20 18:55:04 +0200

    styles: rainbowkit, flag-icons

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -7,6 +7,8 @@ import AppRoot from '~/AppRoot.tsx'
 
 import '@styles/global.css'
 import '@styles/tailwind.css'
+import '@rainbow-me/rainbowkit/styles.css';
+import 'node_modules/flag-icons/css/flag-icons.min.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
