da5a1a6a7ee68ace42809a13853ab4b4c76a8bfc
BenoistP <bp_github.com@gazouille.net>
2023-11-01 10:38:13 +0100

    Update MainContent.tsx

--- frontend/src/app/components/MainContent.tsx
+++ frontend/src/app/components/MainContent.tsx
@@ -30,6 +30,7 @@ export const MainContent = ( ) => {
     // ---
 
     useEffect(() => {
+      console.dir(tokenListsData)
       setTokensLists(tokenListsData) // Set inital token list data
     }, [])
 
