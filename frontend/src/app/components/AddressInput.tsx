097aad06236db0b40b2cff452a103f3a825ad4f7
BenoistP <bp_github.com@gazouille.net>
2023-11-01 20:19:15 +0100

    cleanup

--- frontend/src/app/components/AddressInput.tsx
+++ frontend/src/app/components/AddressInput.tsx
@@ -1,13 +1,10 @@
 // React
 import { useCallback, useEffect, useState} from "react";
-
 // Utils
 import { checkAndFixAddress0xFormat, isValidAddress, checksumAddress } from "@jsutils/blockchainUtils";
-import { /* ADDRESS_MIN_SIZE, */ ADDRESS_MAX_SIZE, /* DEFAULT_TARGET_ADDRESS, */ NULL_ADDRESS } from "@jsconsts/addresses";
-
+import { ADDRESS_MAX_SIZE, NULL_ADDRESS } from "@jsconsts/addresses";
 // Translation
 import { useTranslation } from 'react-i18next';
-
 // Icons
 import { BackspaceIcon, CheckIcon, ExclamationTriangleIcon, InformationCircleIcon, XMarkIcon } from '@heroicons/react/24/solid'
 
@@ -16,27 +13,21 @@ import { BackspaceIcon, CheckIcon, ExclamationTriangleIcon, InformationCircleIco
 const AddressInput = ({
   sourceAddress,
   targetAddress,
-  settargetAddress
-} :IAddressInputProps ) => {
+  settargetAddress } :IAddressInputProps ) =>
+{
 
-  // const { data: address } = useEnsAddress({ name: 'xxx.eth' }) // not supported on gnosis ?
   const { t } = useTranslation();
-  // const [addressInput, setaddressInput] = useState<TAddressString>(targetAddress);
-  // const [addressInput, setaddressInput] = useState<TAddressEmpty>(targetAddress);
   const [addressInput, setaddressInput] = useState<string>(targetAddress);
   const NULLNULL_ADDRESS = NULL_ADDRESS+NULL_ADDRESS
+  // const { data: address } = useEnsAddress({ name: 'xxx.eth' }) // not supported on gnosis ?
 
   // ------------------------------
 
   // console.debug(`AddressInput.tsx render`)
 
   const handleAddress = /* useCallback( */ (e: React.FormEvent<HTMLInputElement>): void /* ) */ => {
-    // console.debug(`AddressInput.tsx handleAddress useCallback`)
-    // console.debug(`AddressInput.tsx handleAddress useCallback e.currentTarget.value: ${e.currentTarget.value}`)
     const addressInput = e.currentTarget.value
-    // const addressSliced = e.currentTarget.value.slice(0, ADDRESS_MAX_SIZE)
     let addressSliced;
-
     // Restrict input size
     if (addressInput.slice(0,4).toLowerCase() == NULLNULL_ADDRESS) {
       // remove 0x0x
@@ -44,20 +35,9 @@ const AddressInput = ({
     } else {
       addressSliced = addressInput.slice(0, ADDRESS_MAX_SIZE)
     }
-    // console.debug(`AddressInput.tsx handleAddress useCallback addressSliced: ${addressSliced}`)
-/* 
-    if (addressSliced.length < DEFAULT_TARGET_ADDRESS.length) {
-      // console.debug(`AddressInput.tsx handleAddress useCallback addressSliced.length < DEFAULT_TARGET_ADDRESS.length`)
-      // console.debug("set DEFAULT 0x")
-      setaddressInput(DEFAULT_TARGET_ADDRESS)
-      return 
-    }
- */
     const addressFixed:TAddressString = checkAndFixAddress0xFormat(addressSliced)
-    // console.debug(`AddressInput.tsx handleAddress useCallback addressFixed: ${addressFixed}`)
     if (addressFixed.length == ADDRESS_MAX_SIZE) {
       const addressFixedChecksummed = checksumAddress(addressFixed)
-      // console.debug(`AddressInput.tsx handleAddress useCallback addressFixedChecksummed: ${addressFixedChecksummed}`)
       if (isValidAddress(addressFixedChecksummed)) {
         setaddressInput(addressFixedChecksummed)
       } else {
@@ -70,64 +50,46 @@ const AddressInput = ({
 
   // ---
 
-  const eraseAddressInput = useCallback( () => {
-    // console.debug(`AddressInput.tsx eraseAddressInput useCallback`)
-    setaddressInput("")
-  } // eraseAddressInput
-  , [] ); // eslint-disable-next-line react-hooks/exhaustive-deps
+  const eraseAddressInput = useCallback( () =>
+    {
+      setaddressInput("")
+    },
+    []
+  ) // eraseAddressInput
+
   // ---
 
-  const isInputAddressValid = useCallback( () => {
-    // console.debug(`AddressInput.tsx isInputAddressValid useCallback`)
-    const isValidAddressInput = isValidAddress(addressInput)
-    // console.debug(`AddressInput.tsx isInputAddressValid useCallback isValidAddressInput: ${isValidAddressInput} addressInput: ${addressInput}`)
-    return isValidAddressInput
-  }, [addressInput] )
-/*   , [] ); // eslint-disable-next-line react-hooks/exhaustive-deps */
+  const isInputAddressValid = useCallback( () =>
+    {
+      const isValidAddressInput = isValidAddress(addressInput)
+      return isValidAddressInput
+    },
+    [addressInput]
+  ) // isInputAddressValid
 
   useEffect( () =>
     {
-      // console.debug(`AddressInput.tsx useEffect`)
       if (addressInput && isValidAddress(addressInput) && sourceAddress!=addressInput) {
           settargetAddress(addressInput as TAddressString)
         } else {
           settargetAddress("")
       }
     },
-    [
-      // addressInput
-      addressInput, settargetAddress, sourceAddress]
-  )
+    [ addressInput, settargetAddress, sourceAddress ]
+  ) // useEffect
 
   // ------------------------------
-/*
-const handleValidate = useCallback(
-  () =>
-    {
-      console.debug(`handleValidate`);
-      // settargetAddress(addressInput)
-    },
-    []
-  ); 
-*/
-
-/*   const clsAddressHelper = (addressInput == "" ? 'text-accent-content': (isInputAddressValid() ? ((sourceAddress && sourceAddress!=addressInput)? 'text-success font-medium' : 'text-warning' ) : 'text-error') )
- */  // const clsValidate = "btn  rounded-btn btn-neutral btn-outline shadow-xl text py-2 px-1 sm:px-1 md:px-2 m-1 md:m-2" + //  disabled:opacity-50 : "hover:bg-gray-100")
-  // " ease-in-out duration-300 sm:h-10 md:h-12" +
-  // " disabled:btn disabled:btn-disabled disabled:cursor-no-drop"
 
   // 0: info // 1: success // 2: warning // 3: error
   const inputStatus:number = (addressInput == "" ? 0 : ( isInputAddressValid() ? ( (sourceAddress && sourceAddress!=addressInput) ? 1 : 2 ) : 3) );
-  // console.info(`AddressInput.tsx inputStatus: ${inputStatus}`)
   const clsAddressTextContentHelper = ( inputStatus == 0 ? 'text-info-content': (inputStatus == 1 ? 'text-success-content' : (inputStatus == 2 ? 'text-warning-content' : 'text-error-content') ) ) ;
   const clsAddresBgHelper = ( inputStatus == 0 ? 'bg-info': (inputStatus == 1 ? 'bg-success' : (inputStatus == 2 ? 'bg-warning' : 'bg-error') ) ) ;
-  const clsAddressIconHelper = 'w-4 h-4 sm:w-5 sm:h-5 md:w-6 md:h-6 stroke-2' // + inputStatus 
-  // const clsInput = "input input-bordered w-full max-w-xs input-xs sm:input-sm md:input-md " + ( (addressInput == DEFAULT_TARGET_ADDRESS || addressInput == null) ? 'input-info' : (isInputAddressValid() ? 'input-success' : 'input-error') ) 
+  const clsAddressIconHelper = 'w-4 h-4 sm:w-5 sm:h-5 md:w-6 md:h-6 stroke-2'
   const clsInput = "input join-item tracking-tight ring ring-neutral border-1 input-neutral text-left w-80 input-xs sm:input-sm sm:w-96 md:input-md text-accent-content m-1 " + ( (addressInput == "" || addressInput == null) ? 'border-info' : (isInputAddressValid() ? ((sourceAddress && sourceAddress!=addressInput)? 'border-success' : 'border-warning' ) : 'border-error') )
 
   return (
 
-      <div className={"w-full transition-all rounded-lg m-0 p-1 justify-start items-start " + clsAddresBgHelper}> {/* bg-base-100 */}
+      <div className={"w-full transition-all rounded-lg m-0 p-1 justify-start items-start " + clsAddresBgHelper}>
 
         <div className="flex flex-wrap">
 
@@ -136,17 +98,11 @@ const handleValidate = useCallback(
                 <input
                   type="text" placeholder={ t("moveTokens.stepOne.destinationPlaceholder")}
                   className={clsInput}
-                  /* minWidth={ADDRESS_MAX_SIZE} maxWidth={ADDRESS_MAX_SIZE} */
-                  // min={ADDRESS_MIN_SIZE} max={ADDRESS_MAX_SIZE}
                   size={ADDRESS_MAX_SIZE} minLength={ADDRESS_MAX_SIZE} maxLength={ADDRESS_MAX_SIZE}
                   value={addressInput}
                   onChange={ (e) => {handleAddress(e)} }
                   spellCheck="false"
                 />
-                {/* <label className="label">
-                  <span className="label-text-alt">Bottom Left label</span>
-                  <span className="label-text-alt">Bottom Right label</span>
-                </label> */}
                 <button
                   className="btn btn-xs join-item min-h-full ring ring-inset ring-neutral"
                   onClick={ () => eraseAddressInput()}
@@ -154,14 +110,12 @@ const handleValidate = useCallback(
                   <BackspaceIcon className="w-4 h-4 sm:w-5 sm:h-5 md:w-6 md:h-6  " />
                 </button>
               </div>
-              
             </div>
 
           <div className="flex rounded-lg items-center">
 
             <div className="   ">
               <label className={"label h-full m-0 p-2 label-text text-xs sm:text-sm lg:text-base " + clsAddressTextContentHelper }>
-            
                   { (addressInput == "" || addressInput == null) &&
                     <>
                       <InformationCircleIcon className={clsAddressIconHelper} /> &nbsp;{t("moveTokens.stepOne.detinationEmpty")}
@@ -185,8 +139,6 @@ const handleValidate = useCallback(
                       &nbsp;{t("moveTokens.stepOne.destinationError")}
                     </>
                   }
-            
-                {/* <span className="label-text-alt">Top Right label</span> */}
               </label>
 
             </div>
