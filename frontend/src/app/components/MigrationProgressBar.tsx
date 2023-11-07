ce7a8af05bc991a39dd2d4b2f6f4378603d8ded6
BenoistP <bp_github.com@gazouille.net>
2023-11-07 20:27:42 +0100

    style

--- frontend/src/app/components/MigrationProgressBar.tsx
+++ frontend/src/app/components/MigrationProgressBar.tsx
@@ -10,10 +10,47 @@ const MigrationProgressBar = ({ showProgressBar=false, migrationState }: ITF_Pro
   return (
       <div className="w-full h-full ">
         {/* <progress className="progress w-full"></progress> */}
-        <progress className="progress progress-info w-full" value={migrationState.errorItemsCount+migrationState.skippedItemsCount+migrationState.successItemsCount} max={migrationState.totalItemsCount}></progress>
-        <progress className="progress progress-success w-full" value={migrationState.successItemsCount} max={migrationState.totalItemsCount}></progress>
-        <progress className="progress w-full" value={migrationState.skippedItemsCount} max={migrationState.totalItemsCount}></progress>
-        <progress className="progress progress-error w-full" value={migrationState.errorItemsCount} max={migrationState.totalItemsCount}></progress>
+{/*         
+        <div className="flex w-full">
+          <div className="px-2">
+            Progress: {migrationState.errorItemsCount+migrationState.skippedItemsCount+migrationState.successItemsCount} / {migrationState.totalItemsCount}
+          </div>
+          <div className="flex-grow">
+            <progress className="progress progress-info w-full" value={migrationState.errorItemsCount+migrationState.skippedItemsCount+migrationState.successItemsCount} max={migrationState.totalItemsCount}></progress>
+          </div>
+        </div>
+ */}
+        <div className="flex w-full pr-2">
+          <div className="px-2">
+            Succes: {migrationState.successItemsCount} / {migrationState.totalItemsCount}
+          </div>
+          <div className="flex-grow">
+          <progress className="progress progress-success w-full" value={migrationState.successItemsCount} max={migrationState.totalItemsCount}></progress>
+          </div>
+        </div>
+
+        <div className="flex w-full pr-2">
+          <div className="px-2">
+            Skipped: {migrationState.skippedItemsCount} / {migrationState.totalItemsCount}
+          </div>
+          <div className="flex-grow">
+            <progress className="progress w-full" value={migrationState.skippedItemsCount} max={migrationState.totalItemsCount}></progress>
+          </div>
+        </div>
+
+        <div className="flex w-full pr-2">
+          <div className="px-2">
+            Failed: {migrationState.errorItemsCount} / {migrationState.totalItemsCount}
+          </div>
+          <div className="flex-grow">
+            <progress className="progress progress-error w-full" value={migrationState.errorItemsCount} max={migrationState.totalItemsCount}></progress>
+          </div>
+        </div>
+
+        <div className="flex-grow px-2">
+            <progress className="progress progress-info w-full" value={migrationState.errorItemsCount+migrationState.skippedItemsCount+migrationState.successItemsCount} max={migrationState.totalItemsCount}></progress>
+        </div>
+
       </div>
   );
 };
