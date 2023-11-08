a13ad76d9e505194f93b5df280324ed191b96ddb
BenoistP <bp_github.com@gazouille.net>
2023-11-08 20:16:34 +0100

    Coming soon

--- frontend/src/app/components/Step3.tsx
+++ frontend/src/app/components/Step3.tsx
@@ -2,6 +2,8 @@
 import { useEffect } from "react";
 // Components
 import TokenInstanceMigrationListTable from "@Components/TokenInstanceMigrationListTable";
+// Icons
+import { FaceSmileIcon } from '@heroicons/react/24/solid'
 
 // ------------------------------
 
@@ -31,14 +33,28 @@ const Step3 = ( {
 
   return (
     <>
-      <div className="w-full p-0 m-0">
-          <TokenInstanceMigrationListTable
-            // chainId={chainId}
-            tokensInstances={tokensInstances}
-            accountAddress={accountAddress}
-            targetAddress={targetAddress}
-            tokensInstancesListTablePropsHandlers={tokensInstancesListTablePropsHandlers}
-          />
+      <div className="w-full block p-2">
+
+        <div className="bg-neutral w-full rounded-box border border-base-300-content p-2">
+          <p className="text-sm sm:text-base md:text-lg overflow-hidden text-center text-accent p-2 pb-0">
+            {"COMING"}
+          </p>&nbsp;
+          <div className="flex text-neutral-content justify-center p-0">
+            <FaceSmileIcon className="w-8 h-8 sm:w-10 sm:h-10 md:w-12 md:h-12  " />
+          </div>
+          <p className="text-sm sm:text-base md:text-lg overflow-hidden text-center text-warning font-extrabold p-2">
+            {"SOON"}
+          </p>
+        </div>
+        <div className="w-full p-0 m-0">
+            <TokenInstanceMigrationListTable
+              // chainId={chainId}
+              tokensInstances={tokensInstances}
+              accountAddress={accountAddress}
+              targetAddress={targetAddress}
+              tokensInstancesListTablePropsHandlers={tokensInstancesListTablePropsHandlers}
+            />
+        </div>
       </div>
   </>
   );
