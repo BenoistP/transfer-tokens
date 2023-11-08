6b252d0999bb6b668bfcb0562b64972f81e25081
BenoistP <bp_github.com@gazouille.net>
2023-11-08 21:56:22 +0100

    Update Step3.tsx

--- frontend/src/app/components/Step3.tsx
+++ frontend/src/app/components/Step3.tsx
@@ -36,13 +36,13 @@ const Step3 = ( {
       <div className="w-full block p-2">
 
         <div className="bg-neutral w-full rounded-box border border-base-300-content p-2">
-          <p className="text-sm sm:text-base md:text-lg overflow-hidden text-center text-accent p-2 pb-0">
+          <p className="text-sm sm:text-base md:text-lg overflow-hidden text-center text-accent p-2 pb-0  animate-pulse">
             {"COMING"}
           </p>&nbsp;
           <div className="flex text-neutral-content justify-center p-0">
-            <FaceSmileIcon className="w-8 h-8 sm:w-10 sm:h-10 md:w-12 md:h-12  " />
+            <FaceSmileIcon className="w-8 h-8 sm:w-10 sm:h-10 md:w-12 md:h-12  animate-pulse " />
           </div>
-          <p className="text-sm sm:text-base md:text-lg overflow-hidden text-center text-warning font-extrabold p-2">
+          <p className="text-sm sm:text-base md:text-lg overflow-hidden text-center text-warning font-extrabold p-2  animate-pulse">
             {"SOON"}
           </p>
         </div>
