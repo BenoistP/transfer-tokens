7a80642c746149d8b9794ea4f8504e5299144f19
BenoistP <bp_github.com@gazouille.net>
2023-11-08 20:37:47 +0100

    i18n for MigrationProgressBar

--- frontend/src/app/components/MigrationProgressBar.tsx
+++ frontend/src/app/components/MigrationProgressBar.tsx
@@ -1,10 +1,13 @@
 // React
 // import { useEffect, useState } from "react";
+// Translation
+import { useTranslation } from "react-i18next";
 
 // ------------------------------
 
 const MigrationProgressBar = ({ migrationState }: ITF_TransferProgressBar ) => {
 
+  const { t } = useTranslation();
   // console.debug(`MigrationProgressBar.tsx render migrationState=`)
   // console.dir(migrationState)
 
@@ -41,16 +44,16 @@ const MigrationProgressBar = ({ migrationState }: ITF_TransferProgressBar ) => {
 
         <div className="flex w-full pr-2">
           <div className="px-2">
-            Succes: {migrationState.successItemsCount} / {migrationState.totalItemsCount}
+            {t('moveTokens.stepThree.migrationProgressBar.success')}: {migrationState.successItemsCount} / {migrationState.totalItemsCount}
           </div>
           <div className="flex-grow">
-          <progress className="progress progress-success w-full" value={migrationState.successItemsCount} max={migrationState.totalItemsCount}></progress>
+            <progress className="progress progress-success w-full" value={migrationState.successItemsCount} max={migrationState.totalItemsCount}></progress>
           </div>
         </div>
 
         <div className="flex w-full pr-2">
           <div className="px-2">
-            Skipped: {migrationState.skippedItemsCount} / {migrationState.totalItemsCount}
+            {t('moveTokens.stepThree.migrationProgressBar.skipped')}: {migrationState.skippedItemsCount} / {migrationState.totalItemsCount}
           </div>
           <div className="flex-grow">
             <progress className="progress w-full" value={migrationState.skippedItemsCount} max={migrationState.totalItemsCount}></progress>
@@ -59,7 +62,7 @@ const MigrationProgressBar = ({ migrationState }: ITF_TransferProgressBar ) => {
 
         <div className="flex w-full pr-2">
           <div className="px-2">
-            Failed: {migrationState.errorItemsCount} / {migrationState.totalItemsCount}
+            {t('moveTokens.stepThree.migrationProgressBar.failed')}: {migrationState.errorItemsCount} / {migrationState.totalItemsCount}
           </div>
           <div className="flex-grow">
             <progress className="progress progress-error w-full" value={migrationState.errorItemsCount} max={migrationState.totalItemsCount}></progress>
@@ -68,10 +71,10 @@ const MigrationProgressBar = ({ migrationState }: ITF_TransferProgressBar ) => {
 
         <div className="flex w-full pr-2">
           <div className={"px-2 text-base font-semibold "+(processed==migrationState.totalItemsCount?"text-info":"text-warning")}>
-            Status: {processed==migrationState.totalItemsCount?"Completed":"In progress"}
+            {t('moveTokens.stepThree.migrationProgressBar.status.label')}: {processed!=migrationState.totalItemsCount?t('moveTokens.stepThree.migrationProgressBar.status.progress'):t('moveTokens.stepThree.migrationProgressBar.status.completed')}
           </div>
           <div className="flex-grow">
-            <progress className="progress progress-info w-full" value={processed}></progress>
+            <progress className="progress progress-info w-full" value={processed} max={migrationState.totalItemsCount}></progress>
           </div>
         </div>
 
