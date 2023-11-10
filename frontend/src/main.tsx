72b95b49e8b0cfdd6189a1ac66145752081df6d2
BenoistP <75934369+BenoistP@users.noreply.github.com>
2023-11-10 12:44:36 +0100

    cleanup

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,14 +1,12 @@
+// React
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-
+// Components
 import AppRoot from '@App/AppRoot.tsx'
-
-
+// Styles
 import '@styles/global.css'
 import '@styles/tailwind.css'
 import '@rainbow-me/rainbowkit/styles.css';
-// import 'node_modules/flag-icons/css/flag-icons.min.css'
-// import 'flag-icon/css/flag-icon.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
