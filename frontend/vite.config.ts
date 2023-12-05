729e0c7018b6d024d2b62531303870caf0580c7b
BenoistP <75934369+BenoistP@users.noreply.github.com>
2023-12-05 21:02:57 +0100

    Misc projects updates (#67)
    * clean token list sources
    
    * Update README.md
    
    * disclaimer
    
    * format source
    
    * Rainbow wallet SHOULD connect on Gnosis first
    
    * Update TODO.md
    
    * initialChain: Gnosis
    
    * Update vite.config.ts
    
    split build into smaller chunks
    
    * modules version update (#65)
    
    * Update vite.config.ts
    
    split build into smaller chunks: runtime errors fixes

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -10,29 +10,10 @@ export default defineConfig({
 			output: {
 				manualChunks(id) {
 					if (id.includes('node_modules')) {
-						if (id.includes('i18n')) {
-							return 'i18next-vendor'
-						} else if (id.includes('rainbow-me')) {
-							if (id.includes('walletConnectors')) return 'rainbow-me-connectors-vendor'
-							if (id.includes('chunk')) return 'rainbow-me-misc-vendor'
-							return 'rainbow-me-vendor'
-						} else if (id.includes('wagmi')) {
-							return 'wagmi-vendor'
-						} else if (id.includes('dicebear') || id.includes('heroicons') || id.includes('country-flag-icons')) {
+						if (id.includes('dicebear') || id.includes('heroicons') || id.includes('country-flag-icons')) {
 							return 'icons-vendor'
-						} else if (id.includes('viem')) {
-							return 'viem-vendor'
-						} else if (id.includes('react-router-dom')) {
-							return 'react-router-dom-vendor'
-						} else if (id.includes('react-dom')) {
-							return 'react-dom-vendor'
-						} else if (id.includes('lodash')) {
-							return 'lodash-vendor'
-						} else if (id.includes('walletconnect') || id.includes('@safe')) {
-							return 'wallets-vendor1'
-						} else if (id.includes('metamask') || id.includes('@coinbase')) {
-							return 'wallets-vendor2'
-						} else if (
+						}
+						if (
 							id.includes('sha') ||
 							id.includes('eth-') ||
 							id.includes('keccak') ||
@@ -41,25 +22,24 @@ export default defineConfig({
 							id.includes('bignumber') ||
 							id.includes('multiformats') ||
               id.includes('@noble')
-						) {
-							return 'misc-crypto-vendor'
-						} else if (id.includes('qrcode')) {
-							return 'qrcode-vendor'
-						} else if (id.includes('react')) {
-							return 'react-vendor'
-						} else if (id.includes('rxjs')) {
-							return 'rxjs-vendor'
-						} else if (id.includes('motionone') || id.includes('semver') || id.includes('base64') || id.includes('uuid')) {
-							return 'misc-vendor1'
+						) return 'misc-crypto-vendor'
+						if (id.includes('i18n')) {
+							return 'i18next-vendor'
 						}
-            // console.debug("id", id);
-            return 'global-vendor'
+						if (id.includes('rainbow-me')) {
+							if (id.includes('walletConnectors')) return 'rainbow-me-connectors-vendor'
+							if (id.includes('chunk')) return 'rainbow-me-chunks-vendor'
+							return 'rainbow-me-vendor'
+						}
+						if (id.includes('lodash')) {
+							return 'lodash-vendor'
+						}
 					}
 				},
+
 			},
 		},
 	},
-
 	plugins: [react(), tsconfigPaths()],
 	preview: {
 		port: 8000, // serve BUILD
